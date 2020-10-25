c30841ca5daf34df657582bc5cff0e95681ddc39
Sol Orive <sol.orive@mercadolibre.com>
2020-10-24 22:00:22 -0300

    First

---
+++ FiuMark - Análisis.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + pycharm={"is_executing": false}
+print('Hello, world!')
+# -
+
+
